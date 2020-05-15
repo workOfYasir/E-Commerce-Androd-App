61c051df369b9b95b0efc440f7016c9af394291a
Asim Zahid <asimzahid02@gmail.com>
2020-05-15 18:26:39 +0500

    Register Activity

--- app/src/main/java/io/simstech/ecommerceapp/MainActivity.java
+++ app/src/main/java/io/simstech/ecommerceapp/MainActivity.java
@@ -2,7 +2,9 @@ package io.simstech.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
